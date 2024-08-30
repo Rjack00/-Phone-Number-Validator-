562b27f7b1a6313eb6687e27b592f0a8b9afcf65
Rjack00 <rjackson77@comcast.net>
2024-08-29 18:43:56 -0600

    script.js updated
    working to get regex to test true

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ const userInput = document.getElementById("user-input");
 const resultsDiv = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
+const userValue = userInput.value;
 
-const regex = /\d{10}|\d{3}-\d{3}-\d{4}|^[^\s]1?\s?\(\d{3}\)[-\s]?\d{3}[-\s]\d{4}|1\s\d{3}[-\s]\d{3}[-\s]\d{4} /;
+const regex = /\d{10}|\d{3}-\d{3}-\d{4}|^[^\s]1?\s?\(\d{3}\)[-\s]?\d{3}[-\s]\d{4}|1\s\d{3}[-\s]\d{3}[-\s]\d{4}/;
 
 
-checkBtn.addEventListener("click", (e) => {
-    if(userInput.value === "") {
+const numTest = regex.test(userValue);
+
+const numberCheck = () => {
+    console.log(numTest, userInput.value);
+    if (userInput.value === "") {
         return alert("Please provide a phone number");
     }
 
-})
+    if (numTest) {
+        resultsDiv.innerHTML = `
+            <h4 class="valid">Valid US number:</h4>
+            <h4 class="valid">${userInput.value}</h4>`;
+    } else {
+        resultsDiv.innerHTML = `
+            <h4 class="invalid">Invalid US number:</h4>
+            <h4 class="invalid">${userInput.value}</h4>`;
+    }
 
-clearBtn.addEventListener("click", () => resultsDiv.textContent = ""
-)
+    userInput.value = "";
+}
+
+checkBtn.addEventListener("click", () => numberCheck())
+
+clearBtn.addEventListener("click", () => resultsDiv.textContent = "")
