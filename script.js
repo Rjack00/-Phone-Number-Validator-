af916c256b4ddcf424d0547234d70c8b6b3b2e82
Rjack00 <rjackson77@comcast.net>
2024-09-02 16:17:17 -0600

    improved regex efficiency

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const resultsDiv = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 
-const regex = /^\d{10}$|^[^\s]1?\s?(\d{3})([-\s]?)(\d{3}[-\s]?\d{4})|^[^\s]1?\s?\(\1\)\2\3/;
-
-// const regex2 = /(\d{3})\1/;
+const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}$/;  
 
 const numberCheck = (phoneNumber) => {
     const numTest = regex.test(phoneNumber);
@@ -16,12 +14,12 @@ const numberCheck = (phoneNumber) => {
 
     if (numTest) {
         resultsDiv.innerHTML = `
-            <h4 class="valid">Valid US number:</h4>
-            <h4 class="valid">${phoneNumber}</h4>`;
+            <h4 class="valid">Valid US<br> Phone Number:</h4>
+            <h4 class="valid number">${phoneNumber}</h4>`;
     } else {
         resultsDiv.innerHTML = `
-            <h4 class="invalid">Invalid US number:</h4>
-            <h4 class="invalid">${phoneNumber}</h4>`;
+            <h4 class="invalid">Invalid US <br>Phone Number:</h4>
+            <h4 class="invalid number">${phoneNumber}</h4>`;
     }
 
     userInput.value = "";
@@ -31,4 +29,3 @@ checkBtn.addEventListener("click", () => numberCheck(userInput.value))
 
 clearBtn.addEventListener("click", () => resultsDiv.textContent = "")
 
-// 1(234) 567 8910
